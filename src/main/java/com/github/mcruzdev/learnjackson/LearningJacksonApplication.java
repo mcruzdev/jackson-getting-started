9e01c54c99182aa8b9ef58d5d18dbb0f6465d8d3
mcruzdev <matheuscruz.dev@gmail.com>
2021-01-16 15:33:55 -0300

    first commit

---
+++ src/main/java/com/github/mcruzdev/learnjackson/LearningJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.github.mcruzdev.learnjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJacksonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mcruzdev/learnjackson/LearningJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mcruzdev.learnjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
